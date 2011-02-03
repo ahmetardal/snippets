c9e6bca25a6643dc0ba2878042eac87862f5cabc
Ahmet Ardal <ardalahmet@gmail.com>
2011-02-03 14:57:36 +0200

    added UIScreen+Utils.m category class

---
+++ obj-c/UIScreen+Utils.h
@@ -0,0 +1,14 @@
+//
+//  UIScreen+Utils.h
+//  LiveGO
+//
+//  Created by Ahmet Ardal on 2/3/11.
+//  Copyright 2011 MessengerFX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UIScreen(Utils)
+- (BOOL) isRetina;
+@end
---
+++ obj-c/UIScreen+Utils.m
@@ -0,0 +1,19 @@
+//
+//  UIScreen+Utils.m
+//  LiveGO
+//
+//  Created by Ahmet Ardal on 2/3/11.
+//  Copyright 2011 MessengerFX. All rights reserved.
+//
+
+#import "UIScreen+Utils.h"
+
+
+@implementation UIScreen(Utils)
+
+- (BOOL) isRetina
+{
+    return [self respondsToSelector:@selector(scale)] && ([self scale] == 2.0f);
+}
+
+@end
